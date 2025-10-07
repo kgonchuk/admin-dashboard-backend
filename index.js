9567d66036d456a8f3ce9cc5e7009fd11a348654
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2025-10-07 09:46:15 +0300

    change cors

--- index.js
+++ index.js
@@ -3,13 +3,27 @@
 import 'dotenv/config';
 // import './db/index.js'; //connect to db
 import './db.js';
+import cors from "cors";
 
 import express from 'express';
+
 const app = express()
 import routes from './routes/index.js';
 app.use(express.json()); 
 
 app.use("/api",routes )
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  credentials: true
+}));
+app.use(function (req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
 
 //handle 404
 app.use((req, res, next) => {
