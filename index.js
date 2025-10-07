c5b7b905bcec7329a3f6ba4684d0187d9844c40f
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2025-10-07 09:52:30 +0300

    port

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ app.use(express.json());
 
 app.use("/api",routes )
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
   credentials: true
 }));
 app.use(function (req, res, next) {
 
-  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+  res.header('Access-Control-Allow-Origin', "http://localhost:8080");
   res.header('Access-Control-Allow-Headers', true);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
