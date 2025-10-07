c4109725331a907971ed7aeac56f62be5f26bfd1
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2025-10-07 10:16:33 +0300

    change routes

--- index.js
+++ index.js
@@ -9,19 +9,21 @@ const app = express()
 import routes from './routes/index.js';
 app.use(express.json()); 
 
-app.use("/api/user",routes )
+app.use("/api",routes )
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-  credentials: true
-}));
-app.use(function (req, res, next) {
+  origin: ['http://localhost:3000', 'https://admin-dashboard-backend-1-76pt.onrender.com']}));
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+//   credentials: true
+// }));
+// app.use(function (req, res, next) {
 
-  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
-  res.header('Access-Control-Allow-Headers', true);
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  next();
-});
+//   res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+//   res.header('Access-Control-Allow-Headers', true);
+//   res.header('Access-Control-Allow-Credentials', true);
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   next();
+// });
 app.get("/", (req, res) => {
   res.send("api is alive hello")
 })
