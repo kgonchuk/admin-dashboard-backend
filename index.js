0ee1beeb271d489b383a53b294a537748ee947e9
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2025-10-07 10:01:47 +0300

    fix render config

--- index.js
+++ index.js
@@ -13,17 +13,20 @@ app.use(express.json());
 
 app.use("/api",routes )
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://127.0.0.1:8080'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
   credentials: true
 }));
 app.use(function (req, res, next) {
 
-  res.header('Access-Control-Allow-Origin', "http://localhost:8080");
+  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
   res.header('Access-Control-Allow-Headers', true);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   next();
 });
+app.get("/", (req, res) => {
+  res.send("api is alive hello")
+})
 
 //handle 404
 app.use((req, res, next) => {
@@ -34,8 +37,8 @@ app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).send("Something broke!")
 })
-
-app.listen(8080, () => {
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
   console.log("Server started on port 8080")
 })
 
