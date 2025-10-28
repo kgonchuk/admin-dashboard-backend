dd118d3722a7e2a90e44c0f57682b64e355be728
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2025-10-28 17:16:43 +0200

    order fix

--- models/order.js
+++ models/order.js
@@ -3,11 +3,8 @@ import mongoose from 'mongoose';
 const orderSchema = new mongoose.Schema({
     photo: { type: String },
     name: { type: String, required: true },
-    address: {
-    type: String,
-    required: true},
+   
     products: { type: String, required: true },
-    date:String,
     date: { type: Date, required: true },
     price: { type: Number, required: true },
     status:{ type: String, enum: ['Completed', 'Confirmed', 'Pending', 'Cancelled', 'Processing'], required: true  },
