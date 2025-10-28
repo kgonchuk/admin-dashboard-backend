244b91d4fdef027a12aae6d314a07a440eafb773
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2025-10-28 17:17:54 +0200

    fixed order

--- models/order.js
+++ models/order.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const orderSchema = new mongoose.Schema({
     photo: { type: String },
     name: { type: String, required: true },
-   
+    address: {
+    type: String,
+    required: true},
     products: { type: String, required: true },
     date: { type: Date, required: true },
     price: { type: Number, required: true },
